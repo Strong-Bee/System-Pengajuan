609fa48b2cf240024c93040ab7d65b8d257b4485
Strong-Bee <lintangsyahdewo26@gmail.com>
2025-08-30 23:58:14 +0700

    Create Migration

---
+++ database/migrations/2025_08_30_165504_pengajuan.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('pengajuan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('jenis', ['cuti', 'lembur', 'sakit']);
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
+            $table->text('alasan');
+            $table->enum('status', ['pending', 'disetujui', 'ditolak'])->default('pending');
+            $table->text('catatan_admin')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('pengajuan');
+    }
+};
